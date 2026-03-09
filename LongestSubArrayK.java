2968d44a255771bd9c0f49cc3e25e0810c87a6a9
Bhumika <bhumikak401@gmail.com>
2026-03-09 12:31:17 +0530

    Add files via upload

---
+++ LongestSubArrayK.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class LongestSubArrayK {
+    public static void main(String[] args){
+
+        int[] arr = {1,2,3,1,1,1,1,4,2,3};
+        int k = 6;
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int sum = 0, maxLen = 0;
+
+        for(int i=0;i<arr.length;i++){
+
+            sum += arr[i];
+
+            if(sum == k)
+                maxLen = i + 1;
+
+            if(!map.containsKey(sum))
+                map.put(sum,i);
+
+            if(map.containsKey(sum-k))
+                maxLen = Math.max(maxLen, i - map.get(sum-k));
+        }
+
+        System.out.println("Longest Length: " + maxLen);
+    }
+}
---
+++ MajorityElement.java
@@ -0,0 +1,26 @@
+public class MajorityElement {
+    public static int findMajority(int[] arr){
+
+        int candidate = arr[0];
+        int count = 1;
+
+        for(int i=1;i<arr.length;i++){
+            if(arr[i] == candidate)
+                count++;
+            else
+                count--;
+
+            if(count == 0){
+                candidate = arr[i];
+                count = 1;
+            }
+        }
+
+        return candidate;
+    }
+
+    public static void main(String[] args){
+        int[] arr = {2,2,1,1,1,2,2};
+        System.out.println("Majority Element: " + findMajority(arr));
+    }
+}
